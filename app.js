9d566149f49b9d957971ba881f3b6e35ca0479aa
Jesu1701 <jesuscc1701@gmail.com>
2020-10-15 19:10:31 +0200

    Primer ejercicio completo :)

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var myTeam = [{
         name: "Pedro",
         availability: new Array(8).fill(true)
     },
+
     {
         name: "Esther",
         availability: new Array(8).fill(true)
@@ -35,6 +36,7 @@ var myTeam = [{
 // Función para aleatorizar la disponibilidad del equipo
 // Extra: autoejecuta al final
 
+
 var getRandomAvailability = (() => {
     var randomBoolean = () => Math.random() >= 0.5;
     for (var i = 0; i < myTeam.length; i++) {
@@ -42,11 +44,7 @@ var getRandomAvailability = (() => {
         for (var x = 0; x < myTeam[i].availability.length; x++) {
             var saveboolean = randomBoolean()
             myTeam[i].availability[x] = saveboolean;
-            if (myTeam[i].availability[x] === true) {
-                myTeam[i].availability[x] = 'Si';
-            }else {
-                myTeam[i].availability[x] = 'No';
-            }
+
         }
 
     }
@@ -56,13 +54,52 @@ var getRandomAvailability = (() => {
 
 // Función para crear la agenda
 
-var checkAvailability = () => {
-    for (var i = 0; i < myTeam.length; i++) {
-        console.log('Disponibilidad de María', myTeam[i].name)
-        for (var x = 0; x < myTeam[i].availability.length; x++) {
-            console.log(WORK_HOURS[x] + ': ' + myTeam[i].availability[x]);               
-        }
-    }
-}
+var createAgenda = (() => {
 
-console.log(checkAvailability());
+    for (var i = 0; i < myTeam.length; i++) {
+
+        console.log('Disponibilidad de', myTeam[i].name)
+
+        for (var x = 0; x < myTeam[i].availability.length; x++) {
+            myTeam[i].availability[x];
+            if (myTeam[i].availability[x] === true) {
+                console.log(WORK_HOURS[x] + ': ' + 'Si');
+            } else {
+                console.log(WORK_HOURS[x] + ': ' + 'No');
+            }
+        }
+
+    }
+
+})()
+
+
+
+// Función para comparar la agenda de todos los miembros y buscar un hueco donde todos estén disponibles
+
+var output = new Array(8).fill(true);
+var apointment = false;
+
+var compareAgenda = (() => {
+
+    for (var j = 0; j < getRandomAvailability.length; j++) {
+        for (var i = 0; i < getRandomAvailability.length; i++) {
+            output[j] = output[j] && getRandomAvailability[i].availability[j];
+        }
+
+        if (output[j]) {
+            apointment = true;
+            console.log('/*============================*/');
+            console.log('Hay hueco a las', WORK_HOURS[j]);
+            console.log('/*============================*/');
+        }
+
+    }
+    if (apointment === false) {
+        console.log('/*============================*/');
+        console.log('Lo sentimos, no hay hueco disponible');
+        console.log('/*============================*/');
+
+    }
+
+})();

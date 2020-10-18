fb570f50a483f0d8afac98023ac331a555073c18
Jesu1701 <jesuscc1701@gmail.com>
2020-10-18 14:23:00 +0200

    ejercicio 2 completo sin challenge

--- Calculadora de cambio/app.js
+++ Calculadora de cambio/app.js
@@ -1,59 +1,45 @@
 const billetes = [{
-        cantidad: 2,
         importe: 200
     },
     {
-        cantidad: 4,
         importe: 100,
     },
     {
-        cantidad: 3,
         importe: 50,
     },
     {
-        cantidad: 1,
         importe: 20,
     },
     {
-        cantidad: 2,
         importe: 10,
     },
     {
-        cantidad: 1,
         importe: 5,
     },
 ];
 
 const monedas = [{
-        cantidad: 4,
         importe: 2
     },
     {
-        cantidad: 1,
         importe: 1,
     },
     {
-        cantidad: 2,
         importe: 0.50,
     },
     {
-        cantidad: 3,
         importe: 0.20,
     },
     {
-        cantidad: 10,
         importe: 0.10,
     },
     {
-        cantidad: 5,
         importe: 0.05,
     },
     {
-        cantidad: 10,
         importe: 0.02,
     },
     {
-        cantidad: 10,
         importe: 0.01,
     },
 ];
@@ -62,25 +48,29 @@ const monedas = [{
 var inputTotal = document.getElementById('total');
 var inputPaid = document.getElementById('paid');
 var buttonCalculate = document.getElementById('calculate');
-var inputResult = document.getElementById('result');
+var textResultBills = document.getElementById('result-bills');
+var textResultCoins = document.getElementById('result-coins');
 
 var result;
 var refundAmount;
 var refundAmountLog;
+var resultMessage
 
 
 var calculateRefundAmount = () => {
 
     result = inputPaid.value - inputTotal.value;
+
     refundAmount = result;
+
     console.log('Su compra cuesta: ', parseInt(inputTotal.value) + '€');
     console.log('Entrega de: ', parseInt(inputPaid.value) + '€');
 
     // Primer bucle, calcula el cambio en función de los billetes
-  
+
     do {
-    
-        if (result > 0) {
+
+        if (result >= 5) {
 
             console.log('Tenemos que devolver: ', result + '€');
             var i = -1;
@@ -91,27 +81,51 @@ var calculateRefundAmount = () => {
 
                 refundAmount = result / billetes[i].importe;
                 var numberOfBill = (parseInt(refundAmount, 10));
-                result = result - (numberOfBill * billetes[i].importe);  
+                result = result - (numberOfBill * billetes[i].importe);
 
-                if(numberOfBill > 0) {
-                    console.log('Nos dan ' + numberOfBill + ' billetes de: ', billetes[i].importe + '€');                 
-                }                              
 
-            } while (refundAmount < 1)            
-           
+
+                if (numberOfBill > 0) {
+
+
+                    resultMessage = numberOfBill + ' billetes de: ' + billetes[i].importe + '€';
+
+                    console.log(resultMessage);
+
+                    var billInfo = document.createElement("p");
+                    var node = document.createTextNode(resultMessage);
+
+                    billInfo.appendChild(node);
+                    textResultBills.appendChild(billInfo);
+
+                }
+
+
+
+
+            } while (refundAmount < 1)
+
 
         } else {
-            console.log('No hay que devolver nada');
+
+            resultMessage = 'No hay que devolver billetes';
+            console.log(resultMessage);
+            var billInfo = document.createElement("p");
+            var node = document.createTextNode(resultMessage);
+
+            billInfo.appendChild(node);
+            textResultBills.appendChild(billInfo);
         }
 
-    } while (result > 5)
+    } while (result >= 5)
 
     // Segundo bucle, calcula el cambio en función de las monedas
 
     if (result > 0) {
 
         console.log('Tenemos que devolver: ', result + '€');
-        console.log('Ahora vamos a buscar monedas...');        
+        console.log('Ahora vamos a buscar monedas...');
+
         var x = -1;
 
         do {
@@ -119,28 +133,46 @@ var calculateRefundAmount = () => {
             x++;
 
             refundAmount = result / monedas[x].importe;
-            var numberOfBill = (parseInt(refundAmount, 10)); 
-            result = result - (numberOfBill * monedas[x].importe);  
-            console.log('Nos dan ' + numberOfBill + ' monedas de: ', monedas[x].importe + '€');
-            
+            var numberOfBill = (parseInt(refundAmount, 10));
+
+            if (numberOfBill > 0) {
+                result = result - (numberOfBill * monedas[x].importe);
+                resultMessage = numberOfBill + ' monedas de: ' + monedas[x].importe + '€'
+
+                console.log(resultMessage);
+
+                var billInfo = document.createElement("p");
+                var node = document.createTextNode(resultMessage);
+
+                billInfo.appendChild(node);
+                textResultCoins.appendChild(billInfo);
+            }
+
+
             if (result > 0) {
                 console.log('Nos siguen deviendo: ', result + '€');
             } else {
                 console.log('No nos deben nada: ', result + '€');
             }
 
-        } while (result > 0)
-    }
 
+        } while (result > 0)
+    } else {
+
+        resultMessage = 'No nos tienen que devolver monedas';
+
+        var billInfo = document.createElement("p");
+        var node = document.createTextNode(resultMessage);
+
+        billInfo.appendChild(node);
+        textResultCoins.appendChild(billInfo);
+    }
     return result;
 }
 
 
 function calculateFinalDevolution() {
-
     calculateRefundAmount();
-    inputResult.value = result;
-
 }
 
 buttonCalculate.addEventListener('click', calculateFinalDevolution);
